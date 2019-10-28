a78e0273d56b541500bd495d9d26cf7cfaadcf13
Gregorio Alanis-Lobato <g.alanis.lobato@gmail.com>
2019-10-28 14:16:17 +0000

    Updated the repo with new samples and UMAP

--- R/02b_cluster_with_griph.R
+++ R/02b_cluster_with_griph.R
@@ -8,36 +8,21 @@ source("utility_functions.R")
 load("results/raw_norm_DESeq_sfn.RData")
 
 res_griph <- griph_cluster(counts(dds), ClassAssignment = dds$exp_group, 
-<<<<<<< HEAD
                            BatchAssignment = factor(dds$batch),
                            use.par = FALSE, plot = FALSE)
-=======
-                     BatchAssignment = factor(dds$batch),
-                     use.par = FALSE, plot = FALSE)
->>>>>>> 4cab0b77e62cced71c7460c1a0d9f17c9751de04
 
 g.true <- plotGraph(res_griph, fill.type = "true", mark.type = "predicted")
 
 emb <- tibble(x = g.true$y[,1], y = g.true$y[,2], cond = dds$sample_type, 
-<<<<<<< HEAD
               cluster = factor(res_griph$MEMB), btch = dds$batch)
-=======
-              cluster = factor(res_griph$MEMB))
->>>>>>> 4cab0b77e62cced71c7460c1a0d9f17c9751de04
 
 col_pal <- c("#7fbc41", "#7fbc41", "#7fbc41", #Epi
              "#6a51a3", "#6a51a3", #PE
              "#C19A6B", #TE
-<<<<<<< HEAD
              "#fe9929", "#fe9929", "#fe9929", "#d73027", "#d73027", #AI
              "#de75ae", #mTeSR1 matrigel
              "#c994c7", "#c994c7", #mTeSR1 laminin
              "#1f78b4", "#1f78b4", "#1f78b4", "#1f78b4", "#1f78b4", #KSR
-=======
-             "#d73027", "#d73027", "#d73027", "#d73027", #AI
-             "#de75ae", "#de75ae", #mTeSR1
-             "#1f78b4", "#1f78b4", "#1f78b4", "#1f78b4", "#1f78b4", "#9ecae1", #KSR
->>>>>>> 4cab0b77e62cced71c7460c1a0d9f17c9751de04
              "#737373", "#737373", "#737373", "#737373", 
              "#737373", "#737373", "#737373", "#737373", "#737373" #Naive
 )
@@ -45,16 +30,10 @@ col_pal <- c("#7fbc41", "#7fbc41", "#7fbc41", #Epi
 shape_pal <- c(16, 17, 15, #Epi
                16, 17, #PE
                16, #TE
-<<<<<<< HEAD
                16, 17, 8, 15, 18, #AI
                16, #mTeSR1 matrigel
                15, 18, #mTeSR1 laminin
                16, 16, 17, 15, 18, #KSR
-=======
-               16, 17, 15, 18, #AI
-               16, 17, #mTeSR1
-               16, 16, 17, 15, 18, 16, #KSR
->>>>>>> 4cab0b77e62cced71c7460c1a0d9f17c9751de04
                16, 17, 17, 15, 
                18, 18, 17, 15, 18 #Naive
 )
@@ -63,14 +42,7 @@ ggplot(emb, aes(x, y, colour = cond, shape = cond)) +
   geom_point(size = 2.5) +
   scale_colour_manual(values = col_pal) + 
   scale_shape_manual(values = shape_pal) +
-<<<<<<< HEAD
   labs(x = "Dim 1", y = "Dim 2", colour = "", shape = "") + 
   theme_bw(base_size = 15)
 
 save(res_griph, emb, col_pal, shape_pal, file = "results/griph_clustering.RData")
-=======
-  labs(x = "Dim 1", y = "Dim 2", colour = "") + 
-  theme_bw(base_size = 15)
-
-save(res_griph, emb, col_pal, file = "results/griph_clustering.RData")
->>>>>>> 4cab0b77e62cced71c7460c1a0d9f17c9751de04
--- R/03a_deseq_analysis.R
+++ R/03a_deseq_analysis.R
@@ -4,11 +4,7 @@ library(dplyr)
 library(readr)
 source("utility_functions.R")
 
-<<<<<<< HEAD
 load("results/hesc_all.RData")
-=======
-load("results/hesc.RData")
->>>>>>> 4cab0b77e62cced71c7460c1a0d9f17c9751de04
 
 stype <- read_tsv("all_samples.tsv")
 stype <- stype %>% 
@@ -16,26 +12,19 @@ stype <- stype %>%
 
 # Create a DESeq2 object with a more general design
 # Putting all the data together
-<<<<<<< HEAD
 hesc$abundance <- hesc$abundance[, stype$sample_name]
 hesc$counts <- hesc$counts[, stype$sample_name]
 hesc$length <- hesc$length[, stype$sample_name]
-=======
->>>>>>> 4cab0b77e62cced71c7460c1a0d9f17c9751de04
+
 dds <- DESeqDataSetFromTximport(txi = hesc, colData = stype, 
                                 design = ~ general_group)
 
 mito <- grep("^MT-", rownames(dds))
 dds <- dds[-mito, ]
-<<<<<<< HEAD
 ribo <- read_tsv("data/ribosomal.txt")
 dds <- dds[!(rownames(dds) %in% ribo$`Approved Symbol`), ]
+
 load("data/t2g_complete.RData")
-=======
-ribo <- read_tsv("ref_transcriptome/ribosomal.txt")
-dds <- dds[!(rownames(dds) %in% ribo$`Approved Symbol`), ]
-load("ref_transcriptome/t2g_complete.RData")
->>>>>>> 4cab0b77e62cced71c7460c1a0d9f17c9751de04
 pseudo <- t2g[grep("pseudogene", t2g$gene_biotype), ]
 dds <- dds[!(rownames(dds) %in% pseudo$symbol), ]
 dds <- dds[!near(rowSums(counts(dds)), 0), ]
@@ -46,7 +35,6 @@ dds <- DESeq(dds)
 save(dds, file = "results/DESeq_with_contrasts.RData")
 
 # Obtain contrasts of interest and coerce to tibble. The p-values correspond to
-<<<<<<< HEAD
 # the null hypothesis that the expected log2FCs are = 0
 # Epi
 ai_v_epi <- results(dds, contrast = c("general_group", "AI", "Epi"), lfcThreshold = 0)
@@ -128,44 +116,18 @@ write_tsv(t2il_v_pe, path = "dgea/t2iL_v_PE.tsv")
 write_tsv(epi_v_pe, path = "dgea/Epi_v_PE.tsv")
 write_tsv(epi_v_te, path = "dgea/Epi_v_TE.tsv")
 write_tsv(te_v_pe, path = "dgea/TE_v_PE.tsv")
-=======
-# the null hypothesis that the expected log2FCs are >= 0
-ai_v_epi <- results(dds, contrast = c("general_group", "AI", "Epi"), lfcThreshold = 1)
-ai_v_epi <- DESeqRes2tibble(ai_v_epi)
-
-mtesr_v_epi <- results(dds, contrast = c("general_group", "mTeSR1", "Epi"), lfcThreshold = 1)
-mtesr_v_epi <- DESeqRes2tibble(mtesr_v_epi)
-
-ksr_v_epi <- results(dds, contrast = c("general_group", "KSR", "Epi"), lfcThreshold = 1)
-ksr_v_epi <- DESeqRes2tibble(ksr_v_epi)
-
-t2il_v_epi <- results(dds, contrast = c("general_group", "t2iL_Go", "Epi"), lfcThreshold = 1)
-t2il_v_epi <- DESeqRes2tibble(t2il_v_epi)
-
-# Write results to TSV files
-write_tsv(ai_v_epi, path = "results/AI_v_Epi.tsv")
-write_tsv(mtesr_v_epi, path = "results/mTeSR1_vs_Epi.tsv")
-write_tsv(ksr_v_epi, path = "results/KSR_v_Epi.tsv")
-write_tsv(t2il_v_epi, path = "results/t2iL_v_Epi.tsv")
->>>>>>> 4cab0b77e62cced71c7460c1a0d9f17c9751de04
 
 # Identify up- and down-regulated genes based on stringent thresholds and 
 # perform functional enrichment analysis
 library(FunEnrich)
 library(cowplot)
-<<<<<<< HEAD
 library(ggplot2)
 pval <- 0.001
 log2FC <- log2(2)
-=======
-pval <- 0.001
-log2FC <- log2(1)
->>>>>>> 4cab0b77e62cced71c7460c1a0d9f17c9751de04
 
 updw_ai <- funenrich_analysis(ai_v_epi, log2FC, pval, benj = FALSE)
 updw_ksr <- funenrich_analysis(ksr_v_epi, log2FC, pval, benj = FALSE)
 updw_mtesr <- funenrich_analysis(mtesr_v_epi, log2FC, pval, benj = FALSE)
-<<<<<<< HEAD
 updw_mtesrL <- funenrich_analysis(mtesrL_v_epi, log2FC, pval, benj = FALSE)
 updw_t2il <- funenrich_analysis(t2il_v_epi, log2FC, pval, benj = FALSE)
 
@@ -225,56 +187,3 @@ readr::write_tsv(updw_t2il$enr_dw$bp, path = "dgea/dw_t2il_bp.tsv")
 readr::write_tsv(updw_t2il$enr_dw$cc, path = "dgea/dw_t2il_cc.tsv")
 readr::write_tsv(updw_t2il$enr_dw$mf, path = "dgea/dw_t2il_mf.tsv")
 readr::write_tsv(updw_t2il$enr_dw$reactome, path = "dgea/dw_t2il_reac.tsv")
-=======
-updw_t2il <- funenrich_analysis(t2il_v_epi, log2FC, pval, benj = FALSE)
-
-save(updw_ai, updw_ksr, updw_mtesr, updw_t2il, file = "results/perm2_DGE.RData")
-
-# AI
-readr::write_tsv(updw_ai$enr_up$bp, path = "results/up_ai_bp.tsv")
-readr::write_tsv(updw_ai$enr_up$cc, path = "results/up_ai_cc.tsv")
-readr::write_tsv(updw_ai$enr_up$mf, path = "results/up_ai_mf.tsv")
-readr::write_tsv(updw_ai$enr_up$reactome, path = "results/up_ai_reac.tsv")
-
-readr::write_tsv(updw_ai$enr_dw$bp, path = "results/dw_ai_bp.tsv")
-readr::write_tsv(updw_ai$enr_dw$cc, path = "results/dw_ai_cc.tsv")
-readr::write_tsv(updw_ai$enr_dw$mf, path = "results/dw_ai_mf.tsv")
-readr::write_tsv(updw_ai$enr_dw$reactome, path = "results/dw_ai_reac.tsv")
-
-# KSR and 3iL
-readr::write_tsv(updw_ksr$enr_up$bp, path = "results/up_ksr_bp.tsv")
-readr::write_tsv(updw_ksr$enr_up$cc, path = "results/up_ksr_cc.tsv")
-readr::write_tsv(updw_ksr$enr_up$mf, path = "results/up_ksr_mf.tsv")
-readr::write_tsv(updw_ksr$enr_up$reactome, path = "results/up_ksr_reac.tsv")
-
-readr::write_tsv(updw_ksr$enr_dw$bp, path = "results/dw_ksr_bp.tsv")
-readr::write_tsv(updw_ksr$enr_dw$cc, path = "results/dw_ksr_cc.tsv")
-readr::write_tsv(updw_ksr$enr_dw$mf, path = "results/dw_ksr_mf.tsv")
-readr::write_tsv(updw_ksr$enr_dw$reactome, path = "results/dw_ksr_reac.tsv")
-
-# mTeSR1
-readr::write_tsv(updw_mtesr$enr_up$bp, path = "results/up_mtesr_bp.tsv")
-readr::write_tsv(updw_mtesr$enr_up$cc, path = "results/up_mtesr_cc.tsv")
-readr::write_tsv(updw_mtesr$enr_up$mf, path = "results/up_mtesr_mf.tsv")
-readr::write_tsv(updw_mtesr$enr_up$reactome, path = "results/up_mtesr_reac.tsv")
-
-readr::write_tsv(updw_mtesr$enr_dw$bp, path = "results/dw_mtesr_bp.tsv")
-readr::write_tsv(updw_mtesr$enr_dw$cc, path = "results/dw_mtesr_cc.tsv")
-readr::write_tsv(updw_mtesr$enr_dw$mf, path = "results/dw_mtesr_mf.tsv")
-readr::write_tsv(updw_mtesr$enr_dw$reactome, path = "results/dw_mtesr_reac.tsv")
-
-# t2iL+GO and 5iLA
-readr::write_tsv(updw_t2il$enr_up$bp, path = "results/up_t2il_bp.tsv")
-readr::write_tsv(updw_t2il$enr_up$cc, path = "results/up_t2il_cc.tsv")
-readr::write_tsv(updw_t2il$enr_up$mf, path = "results/up_t2il_mf.tsv")
-readr::write_tsv(updw_t2il$enr_up$reactome, path = "results/up_t2il_reac.tsv")
-
-readr::write_tsv(updw_t2il$enr_dw$bp, path = "results/dw_t2il_bp.tsv")
-readr::write_tsv(updw_t2il$enr_dw$cc, path = "results/dw_t2il_cc.tsv")
-readr::write_tsv(updw_t2il$enr_dw$mf, path = "results/dw_t2il_mf.tsv")
-readr::write_tsv(updw_t2il$enr_dw$reactome, path = "results/dw_t2il_reac.tsv")
-
-
-
-
->>>>>>> 4cab0b77e62cced71c7460c1a0d9f17c9751de04
